65216873698f34099c04eca331d42a90c5087422
KrisMolotov <shander200160@mail.ru>
2022-10-18 16:19:31 +0300

    censor v2.0.

--- JavaScript/task_1/censor.js
+++ JavaScript/task_1/censor.js
@@ -1,20 +1,21 @@
 function censor ()
 {
     let pairs = new Map()
+    let string3;
     return function (string1, string2 = undefined)
     {
-        if(string2 !== undefined)
-        {
+        if (string2 !== undefined) {
             pairs.set(string1, string2)
-        }
-        else{
-            for(let pair of pairs)
-            {
-                let key = pair[0]
-                let value = pair[1]
-                string1.replace(key, value, global)
+        } else {
+            for (let pair of pairs) {
+                let search = pair[0]
+                let replaceWith = pair[1]
+                /*string3 = string1.split(search).join(replaceWith) - альтернативный вариант:
+                разбиваем строку на части, затем соединям части через заменяющую строку между частями.*/
+                string3 = string1.replaceAll(search, replaceWith) // метод замены всех вхождений строк напрямую
+                string1 = string3
             }
-            return string1
+            return string3
         }
     }
 }
@@ -22,4 +23,4 @@ function censor ()
 const changeScene = censor();
 changeScene('PHP','JS');
 changeScene('backend', 'frontend')
-console.log(changeScene('PHP is the most popular programming language for backend web-development'));
+console.log(changeScene('PHPis the most popular programming PHP backend language for backend web-developmentbackend'));
