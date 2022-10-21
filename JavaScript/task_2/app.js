4a85dce89ef62a55bce35f97e625c16db3837732
KrisMolotov <shander200160@mail.ru>
2022-10-21 03:54:38 +0300

    add showcase v2.0

--- JavaScript/task_2/app.js
+++ JavaScript/task_2/app.js
@@ -20,7 +20,14 @@ let order = [];
 
 function addToBasket(productId) {
     // TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
-
+    if (order.includes(products[productId - 1]))
+    {
+        alert("Товар уже в корзине!")
+    }
+    else
+    {
+        order.push(products[productId - 1])
+    }
     // TODO: если товар еще не в корзине, добавить его из массива products
 
     // Эти строчки не трогаем, они отвечают за переотрисовку страницы
@@ -30,8 +37,15 @@ function addToBasket(productId) {
 
 function removeFromBasket(productId) {
     // TODO: описать логику удаления товара из корзины
-
-    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    let index
+    for(let i = 0; i < order.length; i += 1)
+    {
+        if(productId === order[i].id)
+        {
+            index = order.indexOf(order[i])
+        }
+    }
+    order.splice(index, 1)
     renderCart();
     rerenderTotalPrice();
 }
@@ -39,10 +53,9 @@ function removeFromBasket(productId) {
 
 function rerenderTotalPrice() {
     // TODO: опишите функционал подсчета общей стоимости заказа
-
-
-    // Не меняйте эту строчку
-    document.getElementById('total').innerText = totalPrice;
+    document.getElementById('total').innerText = order.reduce(function (accumulator, item, index, array) {
+        return accumulator + array[index].price
+    }, 0);
 }
 
 // Этот метод остается без изменений
