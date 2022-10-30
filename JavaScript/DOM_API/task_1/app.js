663d0d9d64936914ed6aabaf9395fd840b8e3cbc
KrisMolotov <shander200160@mail.ru>
2022-10-30 13:46:10 +0300

    DOM API task 1 final

--- JavaScript/DOM_API/task_1/app.js
+++ JavaScript/DOM_API/task_1/app.js
@@ -2,8 +2,3 @@ const title = document.querySelector(".gray")
 const btn = document.querySelector("#btn")
 const subtitle = document.querySelector(".title.gray")
 const sections = document.querySelectorAll("section")
-
-console.log(title)
-console.log(btn)
-console.log(subtitle)
-console.log(sections)
